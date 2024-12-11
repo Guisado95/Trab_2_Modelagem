9d9b5e4103a72b12f2987f8654875fb267ec31b4
Guisado95 <148809425+Guisado95@users.noreply.github.com>
2024-12-10 22:29:59 -0300

    Update parqueDiversao.sql

--- parqueDiversao.sql
+++ parqueDiversao.sql
@@ -2,30 +2,30 @@ DROP DATABASE IF EXISTS parqueDiversao;
 CREATE DATABASE parqueDiversao;
 
 CREATE TABLE Visitantes(
-		id SERIAL PRIMARY KEY,
-		nome TEXT NOT NULL,
-		data_nascimento DATE NOT NULL,
-		email TEXT NOT NULL
-	);
+	id SERIAL PRIMARY KEY,
+	nome TEXT NOT NULL,
+	data_nascimento DATE NOT NULL,
+	email TEXT NOT NULL
+);
 	
-	CREATE TABLE Atracao(
-		id SERIAL PRIMARY KEY,
-		nome TEXT NOT NULL,
-		tipo TEXT NOT NULL,
-		capacidade INTEGER NOT NULL
-	);
+CREATE TABLE Atracao(
+	id SERIAL PRIMARY KEY,
+	nome TEXT NOT NULL,
+	tipo TEXT NOT NULL,
+	capacidade INTEGER NOT NULL
+);
 	
-	CREATE TABLE Ingresso(
-		id SERIAL PRIMARY KEY,
-		Visitantes_id INTEGER REFERENCES Visitantes (id),
-		Atracao_id INTEGER REFERENCES Atracao (id),
-		data_visita DATE NOT NULL
-	);
+CREATE TABLE Ingresso(
+	id SERIAL PRIMARY KEY,
+	Visitantes_id INTEGER REFERENCES Visitantes (id),
+	Atracao_id INTEGER REFERENCES Atracao (id),
+	data_visita DATE NOT NULL
+);
 	
-	CREATE TABLE Funcionario(
-		id SERIAL PRIMARY KEY,
-		nome TEXT NOT NULL,
-		cargo TEXT NOT NULL,
-		salario INTEGER NOT NULL,
-		Atracao_id INTEGER REFERENCES Atracao (id)
-	);
+CREATE TABLE Funcionario(
+	id SERIAL PRIMARY KEY,
+	nome TEXT NOT NULL,
+	cargo TEXT NOT NULL,
+	salario INTEGER NOT NULL,
+	Atracao_id INTEGER REFERENCES Atracao (id)
+);
